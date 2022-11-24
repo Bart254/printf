5f0f48d2a4f1f32eb9175e2b6c17b8ed2d70bdf0
Bart254 <omondibartlett3@gmail.com>
2022-11-24 01:23:01 -0800

    Create a printf function

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a repository tackling the ins and outs of printf function. It is useful to understand how the function operates as well as creating your own variadix functions
+
---
+++ print0.c
@@ -0,0 +1,44 @@
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - prints the arguments passed
+ * @format: syntax of argument
+ * @(...)?: optional arguments
+ *
+ * Return: number of characters
+ */
+int _printf(const char *format, ...)
+{
+	char *str;
+	int n;
+	va_list ap;
+
+	va_start(ap, format);
+	for (n = 0; *format != '\0'; format++)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 's')
+			{
+				str = va_arg(ap, char*);
+				write(1, str, strlen(str));
+				n += strlen(str);
+			}
+			if (*format == 'c')
+			{
+				str = va_arg(ap, char*);
+				write(1, str, 1);
+				n++;
+			}
+		}
+		else
+		{
+			write(1, format, 1);
+			n++;
+		}
+	}
+	return (n);
+}
