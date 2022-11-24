d4f1a991df45c562d3412a48eab9226d0af35f50
Bart254 <omondibartlett3@gmail.com>
2022-11-24 02:16:53 -0800

    reset

--- print0.c
+++ print0.c
@@ -20,20 +20,26 @@ int _printf(const char *format, ...)
 	{
 		if (*format == '%')
 		{
-			format++;
-			if (*format == 's')
+			for (format++; *format != '\0'; format++)
 			{
-				str = va_arg(ap, char*);
-				write(1, str, strlen(str));
-				n += strlen(str);
-			}
-			if (*format == 'c')
-			{
-				str = va_arg(ap, char*);
-				write(1, str, 1);
+				if (*format == 's')
+				{
+					str = va_arg(ap, char*);
+					write(1, str, strlen(str));
+					n += strlen(str);
+					break;
+				}
+				if (*format == 'c')
+				{
+					str = va_arg(ap, char*);
+					write(1, str, 1);
+					n++;
+					break;
+				}
+				write(1, format, 1);
 				n++;
+				}
 			}
-		}
 		else
 		{
 			write(1, format, 1);
