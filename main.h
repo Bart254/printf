ced3d7359e41fe09949829fdb5723c2adabee7c0
Bart254 <omondibartlett3@gmail.com>
2023-01-25 16:44:28 +0300

    update header

--- main.h
+++ main.h
@@ -3,5 +3,8 @@
 #include <stdarg.h>
 int _printf(const char *format, ...);
 int specifier(va_list *ap, const char **format);
+int p_int(va_list *ap, const char **format);
+int p_char(va_list *ap, const char **format);
+int p_str(va_list *ap, const char **format);
 
 #endif
