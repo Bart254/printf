8e30e0b9b7d413c104d2d6e721a1ce3690d803b3
Bart254 <omondibartlett3@gmail.com>
2023-01-25 16:44:52 +0300

    update function

--- print0.c
+++ print0.c
@@ -4,6 +4,89 @@
 #include <stdlib.h>
 
 /**
+ * p_int - prints an integer to stdout
+ * @ap: pointer to argument pointer
+ * @format: pointer to string construct
+ * Return: number of characters printed
+ */
+int p_int(va_list *ap, const char **format)
+{
+	int i, q;
+	char *buffer;
+	char temp;
+	char sign;
+	size_t size = 1, e, r;
+
+	i = va_arg(*ap, int);
+	if (i < 0)
+	{
+		i = 0 - i;
+		sign = '-';
+		if (i == 0)
+			size++;
+	}
+	if (i != 0)
+	{
+		for (q = i; q > 0; q /= 10)
+			size++;
+	}
+	buffer = malloc(size);
+	if (buffer == NULL)
+		return (-1);
+	for (e = 0; e < size; e++, i /= 10)
+	{
+		if (e == size - 1 && sign == '-')
+			buffer[e] = '-';
+		else
+			buffer[e] = i % 10 + '0';
+	}
+	for (e = 0, r = size - 1; r > e; r--, e++)
+	{
+		temp = buffer[e];
+		buffer[e] = buffer[r];
+		buffer[r] = temp;
+	}
+	write(STDOUT_FILENO, buffer, size);
+	*format = *format + 1;
+	free(buffer);
+	return (size);
+}
+
+/**
+ * p_char - prints characters to stdout
+ * @format: pointer to string constant
+ * @ap: copy of the position of ap
+ * Return: Always 1
+ */
+int p_char(va_list *ap, const char **format)
+{
+	char c;
+
+	c = va_arg(*ap, int);
+	write(STDOUT_FILENO, &c, 1);
+	*format = *format + 1;
+	return (1);
+}
+
+/**
+ * p_str - prints string passed to construct
+ * @format: string construct
+ * @ap: pointer to arguments of function
+ * Return: size of string
+ */
+int p_str(va_list *ap, const char **format)
+{
+	char *buffer;
+
+	buffer = va_arg(*ap, char *);
+	if (buffer == NULL)
+		buffer = "(null)";
+	write(STDOUT_FILENO, buffer, strlen(buffer));
+	*format = *format + 1;
+	return (strlen(buffer));
+}
+
+/**
  * specifier - handles conversion specifiers
  * @ap: copy of the position of ap
  * @format: pointer to string constant
@@ -11,44 +94,42 @@
  */
 int specifier(va_list *ap, const char **format)
 {
-	int n_printed = 0;
-	char *buffer;
-	char c;
+	int n_printed, e = 0;
+	char ptr[5] = {'d', 'i', 'c', 's'};
 
-	switch (*((*format) + 1))
+	int (*f[4])(va_list *, const char **) = {p_int,
+		p_int, p_char, p_str};
+	if (*(*format + 1) == '\0')
 	{
-		case '\0':
-			n_printed = -1;
+		n_printed = -1;
+		*format = *format + 1;
+		return (n_printed);
+	}
+	else if (*(*format + 1) == '%')
+	{
+		*format = *format + 1;
+		write(STDOUT_FILENO, "%", 1);
+		*format = *format + 1;
+		n_printed = 1;
+	}
+	while (ptr[e])
+	{
+		if (*(*format + 1) == ptr[e])
+		{
 			*format = *format + 1;
+			n_printed = f[e](ap, format);
 			break;
-		case '%':
-			buffer = "%";
-			write(STDOUT_FILENO, buffer, 1);
-			n_printed++;
-			*format = *format + 2;
-			break;
-		case 'c':
-			c = va_arg(*ap, int);
-			write(STDOUT_FILENO, &c, 1);
-			n_printed++;
-			*format = *format + 2;
-			break;
-		case 's':
-			buffer = va_arg(*ap, char *);
-			if (buffer == NULL)
-				buffer = "(null)";
-			write(STDOUT_FILENO, buffer, strlen(buffer));
-			n_printed = strlen(buffer);
-			*format = *format + 2;
-			break;
-		default:
-			write(STDOUT_FILENO, *format, 1);
-			n_printed++;
-			(*format) = (*format) + 1;
+		}
+		e++;
+	}
+	if (ptr[e] == '\0')
+	{
+		write(STDOUT_FILENO, *format, 1);
+		*format = *format + 1;
+		n_printed = 1;
 	}
 	return (n_printed);
 }
-
 
 /**
  * _printf - prints the arguments passed
